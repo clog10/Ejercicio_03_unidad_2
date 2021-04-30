e06c5d498fa8eaa83e0ed550f12d8fbfb74dfbda
Carlos Loaeza <carlosloaeza100598@gmail.com>
2021-04-30 16:53:01 -0500

    Trabajando en las peticiones

--- src/java/datos/Actividad.java
+++ src/java/datos/Actividad.java
@@ -8,7 +8,6 @@ public class Actividad {
     //Factores de Actividad
 
     //Hombres
-
     public static final double h_sedentario = 1.0;
     public static final double h_ligeramente = 1.11;
     public static final double h_moderadamente = 1.25;
@@ -20,4 +19,25 @@ public class Actividad {
     public static final double m_moderadamente = 1.27;
     public static final double m_muyactiva = 1.45;
 
+    private double tmb;
+    private double kcalorias;
+
+    public double getTmb() {
+        return tmb;
+    }
+
+    public void calculaTmb(double tmb) {
+        
+    }
+
+    public double getKcalorias() {
+        return kcalorias;
+    }
+
+    public void calculaKcalorias(double kcalorias) {
+        this.kcalorias = kcalorias;
+    }
+    
+    
+    
 }
