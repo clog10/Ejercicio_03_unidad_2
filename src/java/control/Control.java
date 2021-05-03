8b7bfcc74ea2920bf358d7d394fbb895664da684
Carlos Loaeza <carlosloaeza100598@gmail.com>
2021-05-03 14:07:59 -0500

    Terminado

--- src/java/control/Control.java
+++ src/java/control/Control.java
@@ -24,6 +24,7 @@ public class Control extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        PrintWriter pw = response.getWriter();
         try (
                 PrintWriter out = response.getWriter()) {
             //vinculo a la pagina a donde se dirigira la respuesta
@@ -31,16 +32,17 @@ public class Control extends HttpServlet {
             String nombre = request.getParameter("usuario");
             String pass = request.getParameter("password");
 
-            if (pass == null || pass.isEmpty() || nombre == null || nombre.isEmpty()) {
-                acceder = request.getRequestDispatcher("index.jsp");
-            }
             if (usuarios.autenticar(nombre, pass)) {
                 dato.setNombre(nombre);
                 acceder = request.getRequestDispatcher("acceso_2.jsp");
+                request.setAttribute("dato", dato);
+                //redireccionamiento a la pagina
+                acceder.forward(request, response);
+            } else {
+                pw.println("<p align=\"center\">Usuario y/o Contraseña incorrectos</p>");
+                acceder = request.getRequestDispatcher("index.jsp");
+                acceder.include(request, response);
             }
-            request.setAttribute("dato", dato);
-            //redireccionamiento a la pagina
-            acceder.forward(request, response);
         }
     }
 
